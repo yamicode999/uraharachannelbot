5c2e0bdc4c599d9b198fe32660c474e20bcc5276
yamicode999 <141847088+yamicode999@users.noreply.github.com>
2024-12-16 21:03:06 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = 16418156
-api_hash = "f8431c21d3b515cfd44d7284271d1307"
+api_id = 23768009
+api_hash = "213c50b464e39d3bf8e3727b0201b7df"
 bot_token = "6371730093:AAFhEzaf0U8OODzDvYIjRiYx1rDcrmnByUs"
 owner_id = 1962438125
-channel_id = "@BangHerAss"
+channel_id = "@testhere"
