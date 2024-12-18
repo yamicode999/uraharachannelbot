603ee9da0cf3d7ab2ba232166ce549ca32f6097b
yamicode999 <141847088+yamicode999@users.noreply.github.com>
2024-12-19 02:07:52 +0900

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ api_hash = "213c50b464e39d3bf8e3727b0201b7df"
 bot_token = "7243798874:AAEthh23sLtjygtt1qUqviw0o7P3pSa90iE"
 owner_id = 1962438125
 channel_id = "@testhere"
-soto_id = -1002288451870
+soto_id = "@soto6M22D"
